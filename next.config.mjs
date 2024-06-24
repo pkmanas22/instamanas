374da9ac3f88e6ec361b8d484ddd04ee9a0ac664
Manas Kumar Pradhan <pkmanas22@gmail.com>
2024-06-24 12:16:24 +0530

    google avatar added

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['platform-lookaside.fbsbx.com', 'avatars.githubusercontent.com', 'instamanas.s3.ap-south-1.amazonaws.com'],
+        domains: ['platform-lookaside.fbsbx.com', 'avatars.githubusercontent.com', 'instamanas.s3.ap-south-1.amazonaws.com', 'lh3.googleusercontent.com'],
     },
 };
 
